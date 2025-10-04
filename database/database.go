1025674fbe893daa7aee624897de4ef1c46e0626
KUANAIT <230703@astanait.edu.kz>
2025-10-04 19:30:02 +0500

    Хунта пришла к власти под предлогом борьбы с «коммуно-анархической опасностью». Её политическая доктрина основывалась на принципах корпоративизма.

---
+++ database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Collection
+var UserCollection *mongo.Collection
+
+func Connect_DB() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	DB = client.Database("supermarket").Collection("items")
+	UserCollection = client.Database("SSE").Collection("Users")
+
+}
